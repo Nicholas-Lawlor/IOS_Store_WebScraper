84d366aad4869fc08822713269217526b0424027
Nicky Lawlor <nicky@Nickys-MacBook-Pro.local>
2022-03-10 14:34:54 +0000

    update app info for second db to run

--- app_info_scraper.py
+++ app_info_scraper.py
@@ -260,7 +260,7 @@ def link_check(privacy_policy_link):
 
 #list_url =["https://apps.apple.com/ie/app/go-nowe-miasto-nad-pilic%C4%85/id1574961647","https://apps.apple.com/ie/app/f3-crossroads-il/id1562088347","https://apps.apple.com/ie/app/face-emojis-2-sticker-pack/id1465897354"]
 
-sqliteConnection = sqlite3.connect('urldata.db')
+sqliteConnection = sqlite3.connect('urldb.db')
 cursor2 = sqliteConnection.cursor()
 sqlite_select_query = """SELECT * from links"""
 cursor2.execute(sqlite_select_query)
